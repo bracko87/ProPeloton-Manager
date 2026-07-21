a57452c9c434421fb4497068eadf6ac755579d06
bracko87 <aleksandarlevnajic@gmail.com>
2026-07-21 20:01:49 +0200

    Export generic replay contracts

---
+++ src/race-replay/index.ts
@@ -0,0 +1,8 @@
+/**
+ * index.ts (race replay)
+ *
+ * Public exports for generic replay view-model contracts and validation.
+ */
+
+export * from './replayTypes'
+export * from './validateReplayStageModel'
