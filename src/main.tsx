59ae36be16670a72cd1b21e7f5ceec727677347d
bracko87 <aleksandarlevnajic@gmail.com>
2026-08-24 21:26:33 +0200

    Sync authenticated language preference

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './shadcn.css'
 import './i18n'
 import App from './App'
+import LanguagePreferenceSync from './components/i18n/LanguagePreferenceSync'
 
 const root = createRoot(document.getElementById('app')!)
-root.render(<App />)
+root.render(
+  <>
+    <LanguagePreferenceSync />
+    <App />
+  </>,
+)
